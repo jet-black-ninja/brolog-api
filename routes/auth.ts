74cef43ccc324d322c489e1c4f4811161f45a0dd
jet_black_ninja <pramodkmr958@gmail.com>
2024-09-16 23:47:59 +0530

    initial commit

---
+++ routes/auth.ts
@@ -0,0 +1,6 @@
+import {Router} from "express";
+import {login_post, check_token} from "../controllers/auth_controller";
+
+export const authRoute = Router();
+authRoute.post("/api/login", login_post);
+authRoute.get("/api/check-token", check_token);
