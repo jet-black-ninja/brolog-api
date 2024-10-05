03274fb6aec7a0870d5641b58851b39572883c20
jet_black_ninja <pramodkmr958@gmail.com>
2024-10-05 19:38:07 +0530

    readme file

--- index.ts
+++ index.ts
@@ -30,8 +30,7 @@ const corsOptions = {
   },
 };
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(logger('dev'));
